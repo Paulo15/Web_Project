1f90669a1d3483572a662b6ae139449033dfb5f0
Raaqss <raquel.silva_@hotmail.com.br>
2019-12-06 20:01:11 -0300

    Servlet form

--- Ponto_Eletronico/src/java/logic/SolicitacaoServlet.java
+++ Ponto_Eletronico/src/java/logic/SolicitacaoServlet.java
@@ -13,6 +13,8 @@ import model.Solicitacao;
 import model.User;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
@@ -37,27 +39,64 @@ public class SolicitacaoServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    @Override
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String paginaDestino="/form.html?MAX_FILE_SIZE=4194304\"";
         try {
 
+            
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            SimpleDateFormat sdf1 = new SimpleDateFormat("HH:mm");
+            
+            
             Conexao con = new Conexao();
-            String Usuario = request.getParameter("txtLogin");
-            String Senha = request.getParameter("txtSenha");
+            String Nome = request.getParameter("txtNome");
+            String Sobrenome = request.getParameter("txtSobrenome");
+            String QtdDias = request.getParameter("txtQtdDias");
+            String DatIni = request.getParameter("txtDatIni");
+            String DatFin = request.getParameter("txtDatFin");
+            String HEntrada = request.getParameter("txtHoraEntrada");
+            String HIAlmoco = request.getParameter("txtHoraIniAlmoco");
+            String HFAlmoco = request.getParameter("txtHoraFinAlmoco");
+            String HSaida = request.getParameter("txtHoraSaida");
+            String Obs = request.getParameter("txtObs");
+                    
+            Timestamp dataInicio = new Timestamp(((java.util.Date)sdf.parse(DatIni)).getTime());
+            Timestamp dataFim = new Timestamp(((java.util.Date)sdf.parse(DatFin)).getTime());
             
-            User UserLogin = new User();
-            UserDAO dao = new UserDAO();
+            Timestamp HoraEn = new Timestamp(((java.util.Date)sdf1.parse(HEntrada)).getTime());
+            Timestamp HoraIAlm = new Timestamp(((java.util.Date)sdf1.parse(HIAlmoco)).getTime());
+            Timestamp HoraFAlm = new Timestamp(((java.util.Date)sdf1.parse(HFAlmoco)).getTime());
+            Timestamp HoraSaida = new Timestamp(((java.util.Date)sdf1.parse(HSaida)).getTime());
             
-            UserLogin.setLogin(Usuario);
-            UserLogin.setPassword(Senha);
-            User result = new User();
-            result = (User) dao.getUser(UserLogin);
             
-            String Teste = "teste";
+            Solicitacao NewSolic = new Solicitacao();
+            SolicitacaoDAO dao = new SolicitacaoDAO();
+            
+            NewSolic.setNomeSolicitante(Nome+Sobrenome);
+            NewSolic.setQtddias(Long.parseLong(QtdDias));
+            NewSolic.setDataInicio(dataInicio);
+            NewSolic.setDataFim(dataFim);
+            NewSolic.setHoraEntrada(HoraEn);
+            NewSolic.setHoraInicioAlmoco(HoraIAlm);
+            NewSolic.setHoraFimAlmoco(HoraFAlm);
+            NewSolic.setHoraSaida(HoraSaida);
+            NewSolic.setObs(Obs);
+           
+           
+            
+            Solicitacao result = new Solicitacao();
+            result = (Solicitacao) dao.getUser(UserLogin);
+             paginaDestino = "/pagina_principal.html";
+             
         } catch (Exception ex) {
             System.out.println("Deu ruim");
             ex.printStackTrace();
+        }
+            finally {
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(paginaDestino);
+            dispatcher.forward(request, response);
         }
     }
 }
